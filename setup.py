22aced29c32a39421f74eaee37b8a41e28854e49
iamsudip <iamsudip@programmer.net>
2014-12-16 20:39:06 +0530

    Initial setup

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
 from setuptools import setup
 
-setup(name='YourAppName',
-      version='1.0',
-      description='OpenShift App',
-      author='Your Name',
-      author_email='example@example.com',
-      url='http://www.python.org/sigs/distutils-sig/',
-#      install_requires=['Django>=1.3'],
-     )
+setup(name = 'conversedme',
+    version = '0.1',
+    description = 'Know your contacts through their email id only',
+    author = 'iamsudip',
+    author_email = 'iamsudip@programmer.net',
+    url = 'http://www.conversed.me',
+    install_requires=[ 'Flask==0.10.1',
+        'Flask-Login==0.2.7',
+        'Flask-WTF==0.9.2'
+        ],
+    )
