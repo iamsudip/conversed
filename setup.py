1b0c46233114864628152829f7d097d72eb0d225
iamsudip <iamsudip@programmer.net>
2014-12-17 13:35:59 +0530

    Fixed requirements

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ setup(name = 'conversedme',
     url = 'http://www.conversed.me',
     install_requires=[ 'Flask==0.10.1',
         'Flask-Login==0.2.7',
-        'Flask-WTF==0.9.2'
+        'Flask-WTF==0.9.2',
+        'dnspython'
         ],
     )
