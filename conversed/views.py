1bd00d41eb8cc89c0bffbe691c741e9810506e10
iamsudip <iamsudip@programmer.net>
2014-12-18 13:40:44 +0530

    Added 404 handler

--- conversed/views.py
+++ conversed/views.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template, request
 
+import requests
+
 from main import application
 from utils import validate
-import requests
+
 
 @application.route('/')
 def index():
@@ -13,7 +15,11 @@ def profile(emailid=None):
     emailid = request.form['emailid']
     emailstatus = validate(emailid)
     if emailstatus:
-        return render_template("404.html")
+        return render_template("sorry.html")
     else:
         data = requests.get("https://vibeapp.co/api/v1/initial_data/?api_key=b2acf1eadef73f4aeda890e0571f3e06&email="+emailid)
         return render_template("data.html")
+
+@application.errorhandler(404)
+def not_found(e):
+    return render_template("404.html"), 404
