a538580a00342b8a0fd2b07fcc63aa438f54883f
iamsudip <iamsudip@programmer.net>
2014-12-19 01:19:45 +0530

    Rolled back changes

--- conversed/main.py
+++ conversed/main.py
@@ -4,14 +4,5 @@ application = Flask(__name__)
 
 from views import *
 
-# A neat hack to add custom functions to jinja
-# def websites(web_list):
-#     return ", ".join([ t['url'] for t in web_list ])
-
-# def need_four_workplaces(workplaces_list):
-#     return workplaces_list[:4]
-
 if __name__ == '__main__':
-    # application.jinja_env.filters['websites'] = websites # A neat hack to add custom functions to jinja
-    # application.jinja_env.filters['need_four_workplaces'] = need_four_workplaces
     application.run(debug=True, host="0.0.0.0", port=8888)
