f600eda4c9759bd8b2815a5441d4196297e61b15
iamsudip <iamsudip@programmer.net>
2015-01-02 21:29:25 +0530

    Added exception handling

--- conversed/views.py
+++ conversed/views.py
@@ -18,16 +18,19 @@ def profile():
     if emailstatus:
         return render_template("sorry.html") # enable js validation currently only server side validation is done
     else:
-        response = requests.get("https://vibeapp.co/api/v1/initial_data/?api_key=b2acf1eadef73f4aeda890e0571f3e06&email="+emailid)
-        if response.status_code == 200:
-            data = json.loads(response.text)
-            if data.get('success'):
-                return render_template("data.html", user=data)
+        try:
+            response = requests.get("https://vibeapp.co/api/v1/initial_data/?api_key=b2acf1eadef73f4aeda890e0571f3e06&email="+emailid)
+            if response.status_code == 200:
+                data = json.loads(response.text)
+                if data.get('success'):
+                    return render_template("data.html", user=data)
+                else:
+                    return render_template("sorry.html")
             else:
                 return render_template("sorry.html")
-        else:
+        except requests.exceptions.ConnectionError:
             return render_template("sorry.html")
-
+            
 @application.errorhandler(404)
 def not_found(e):
     return render_template("404.html"), 404
