68246f5569e68128eea70e060adf01b28a185e6c
iamsudip <iamsudip@programmer.net>
2015-01-07 12:44:29 +0530

    Added new email regex validator for server side validation | RFC2822 complaint regex

--- conversed/utils/validators.py
+++ conversed/utils/validators.py
@@ -12,7 +12,7 @@ def validate(emailid):
 
     :param str message: The email address that needs validation.
     """
-    email = re.match("^[a-zA-Z][\w\.-]*[a-zA-Z0-9]@[a-zA-Z][\w\.-]*[a-zA-Z0-9]\.[a-zA-Z][a-zA-Z\.]*[a-zA-Z]$", emailid)
+    email = re.match("""^(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])$""", emailid)
     if email:
         email_domain = emailid.split('@')[-1]
         if not email_domain:
